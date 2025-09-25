af77694f8182162ad88f20d0e1f4fc0e5b736573
DominicDev <s0l0estudi4r@gmail.com>
2025-09-25 20:04:59 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GORILLAZ';
+export let nameSongs = [
+	"FEEL GOOD INC.", /*1*/
+	"CLINT EASTWOOD", /*2*/
+	"RHINESTONE EYES", /*3*/
+	"DARE", /*4*/
+	"19-2000", /*5*/
+	"STYLO", /*6*/
+	"DIRTY HARRY", /*7*/
+	"KIDS WITH GUNS", /*8*/
+	"HUMILITY", /*9*/
+	"EL MAÑANA", /*10*/
+	"TOMORROW COMES TODAY", /*11*/
+	"SATURNZ BARZ", /*12*/
+	"ANDROMEDA", /*13*/
+	"MELANCHOLY HILL", /*14*/
+	"ROCK THE HOUSE", /*15*/
+	"PAC-MAN (FEAT. SCHOOLBOY Q)", /*16*/
+	"GORILLAZ ON MY MIND", /*17*/
+	"SORCERERZ", /*18*/
+	"LAST LIVING SOULS", /*19*/
+	"TRANZ", /*20*/
+]
